83c78bdc12582e72ec68b4d256ca27461d1aa132
TheRichKids <101007247+Therickkids@users.noreply.github.com>
2024-09-23 17:16:26 -0500

    Add files via upload
    1. Leer el número seri del Disco Duro / CD / DVD
    2. ¿Cuántas unidades de disco tiene?
    3. Inventario general del sistema : Procesadores, RAM, NIC, patches.
    4. Obtener MAC Address.
    5. Acceso al Registro del Sistema. Crear Clave / Leer clave / Borrar clave / Modificar clave.
    6. Obtener los procesos activos / "matar" procesos.

---
+++ acceso_hardware_SO/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace acceso_hardware_SO
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLeerNumeroSerieDisco = new System.Windows.Forms.Button();
+            this.btnContarUnidadesDisco = new System.Windows.Forms.Button();
+            this.btnObtenerInventario = new System.Windows.Forms.Button();
+            this.btnObtenerMacAddress = new System.Windows.Forms.Button();
+            this.btnAccesoRegistro = new System.Windows.Forms.Button();
+            this.btnObtenerProcesosActivos = new System.Windows.Forms.Button();
+            this.btnMatarProceso = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLeerNumeroSerieDisco
+            // 
+            this.btnLeerNumeroSerieDisco.Location = new System.Drawing.Point(77, 294);
+            this.btnLeerNumeroSerieDisco.Name = "btnLeerNumeroSerieDisco";
+            this.btnLeerNumeroSerieDisco.Size = new System.Drawing.Size(171, 23);
+            this.btnLeerNumeroSerieDisco.TabIndex = 0;
+            this.btnLeerNumeroSerieDisco.Text = "LEER #SERIE D";
+            this.btnLeerNumeroSerieDisco.UseVisualStyleBackColor = true;
+            this.btnLeerNumeroSerieDisco.Click += new System.EventHandler(this.btnLeerNumeroSerieDisco_Click);
+            // 
+            // btnContarUnidadesDisco
+            // 
+            this.btnContarUnidadesDisco.Location = new System.Drawing.Point(278, 294);
+            this.btnContarUnidadesDisco.Name = "btnContarUnidadesDisco";
+            this.btnContarUnidadesDisco.Size = new System.Drawing.Size(242, 23);
+            this.btnContarUnidadesDisco.TabIndex = 1;
+            this.btnContarUnidadesDisco.Text = "CONTAR UNIDADES";
+            this.btnContarUnidadesDisco.UseVisualStyleBackColor = true;
+            this.btnContarUnidadesDisco.Click += new System.EventHandler(this.btnContarUnidadesDisco_Click);
+            // 
+            // btnObtenerInventario
+            // 
+            this.btnObtenerInventario.Location = new System.Drawing.Point(562, 294);
+            this.btnObtenerInventario.Name = "btnObtenerInventario";
+            this.btnObtenerInventario.Size = new System.Drawing.Size(156, 23);
+            this.btnObtenerInventario.TabIndex = 2;
+            this.btnObtenerInventario.Text = "INVENTARIO";
+            this.btnObtenerInventario.UseVisualStyleBackColor = true;
+            this.btnObtenerInventario.Click += new System.EventHandler(this.btnObtenerInventario_Click);
+            // 
+            // btnObtenerMacAddress
+            // 
+            this.btnObtenerMacAddress.Location = new System.Drawing.Point(173, 343);
+            this.btnObtenerMacAddress.Name = "btnObtenerMacAddress";
+            this.btnObtenerMacAddress.Size = new System.Drawing.Size(75, 23);
+            this.btnObtenerMacAddress.TabIndex = 3;
+            this.btnObtenerMacAddress.Text = "MAC ADRESS";
+            this.btnObtenerMacAddress.UseVisualStyleBackColor = true;
+            this.btnObtenerMacAddress.Click += new System.EventHandler(this.btnObtenerMacAddress_Click);
+            // 
+            // btnAccesoRegistro
+            // 
+            this.btnAccesoRegistro.Location = new System.Drawing.Point(328, 343);
+            this.btnAccesoRegistro.Name = "btnAccesoRegistro";
+            this.btnAccesoRegistro.Size = new System.Drawing.Size(132, 23);
+            this.btnAccesoRegistro.TabIndex = 4;
+            this.btnAccesoRegistro.Text = "AccesoRegistro";
+            this.btnAccesoRegistro.UseVisualStyleBackColor = true;
+            this.btnAccesoRegistro.Click += new System.EventHandler(this.btnAccesoRegistro_Click);
+            // 
+            // btnObtenerProcesosActivos
+            // 
+            this.btnObtenerProcesosActivos.Location = new System.Drawing.Point(519, 343);
+            this.btnObtenerProcesosActivos.Name = "btnObtenerProcesosActivos";
+            this.btnObtenerProcesosActivos.Size = new System.Drawing.Size(172, 23);
+            this.btnObtenerProcesosActivos.TabIndex = 5;
+            this.btnObtenerProcesosActivos.Text = "PROCESOS ACTIVOS";
+            this.btnObtenerProcesosActivos.UseVisualStyleBackColor = true;
+            this.btnObtenerProcesosActivos.Click += new System.EventHandler(this.btnObtenerProcesosActivos_Click);
+            // 
+            // btnMatarProceso
+            // 
+            this.btnMatarProceso.Location = new System.Drawing.Point(327, 400);
+            this.btnMatarProceso.Name = "btnMatarProceso";
+            this.btnMatarProceso.Size = new System.Drawing.Size(133, 23);
+            this.btnMatarProceso.TabIndex = 6;
+            this.btnMatarProceso.Text = "MatarProceso";
+            this.btnMatarProceso.UseVisualStyleBackColor = true;
+            this.btnMatarProceso.Click += new System.EventHandler(this.btnMatarProceso_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(77, -1);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(641, 289);
+            this.dataGridView1.TabIndex = 7;
+            this.dataGridView1.Click += new System.EventHandler(this.btnObtenerProcesosActivos_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnMatarProceso);
+            this.Controls.Add(this.btnObtenerProcesosActivos);
+            this.Controls.Add(this.btnAccesoRegistro);
+            this.Controls.Add(this.btnObtenerMacAddress);
+            this.Controls.Add(this.btnObtenerInventario);
+            this.Controls.Add(this.btnContarUnidadesDisco);
+            this.Controls.Add(this.btnLeerNumeroSerieDisco);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLeerNumeroSerieDisco;
+        private System.Windows.Forms.Button btnContarUnidadesDisco;
+        private System.Windows.Forms.Button btnObtenerInventario;
+        private System.Windows.Forms.Button btnObtenerMacAddress;
+        private System.Windows.Forms.Button btnAccesoRegistro;
+        private System.Windows.Forms.Button btnObtenerProcesosActivos;
+        private System.Windows.Forms.Button btnMatarProceso;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ acceso_hardware_SO/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Net.NetworkInformation;
+using System.Windows.Forms;
+using System.Management;
+
+namespace acceso_hardware_SO
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLeerNumeroSerieDisco_Click(object sender, EventArgs e)
+        {
+            string cUnidad = "C:"; // Unidad de ejemplo
+            MessageBox.Show(ObtenerNumeroSerieDisco(cUnidad));
+        }
+
+        private void btnContarUnidadesDisco_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Número de unidades de disco: " + nContarUnidadesDisco().ToString());
+        }
+
+        private void btnObtenerInventario_Click(object sender, EventArgs e)
+        {
+            ObtenerInventarioSistema();
+        }
+
+        private void btnObtenerMacAddress_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("MAC Address: " + ObtenerMacAddress());
+        }
+
+        private void btnAccesoRegistro_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Process.Start("regedit.exe");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error al abrir el Editor del Registro: {ex.Message}");
+            }
+        }
+
+        private void btnObtenerProcesosActivos_Click(object sender, EventArgs e)
+        {
+            MostrarProcesosActivos(dataGridView1); // Llamar a la función que llena el DataGridView
+        }
+
+        private void btnMatarProceso_Click(object sender, EventArgs e)
+        {
+            // Matar todos los procesos activos
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                try
+                {
+                    int nIdProceso = Convert.ToInt32(row.Cells["ProcessID"].Value);
+                    Process.GetProcessById(nIdProceso).Kill();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error al matar el proceso con ID {row.Cells["ProcessID"].Value}: {ex.Message}");
+                }
+            }
+            // Actualizar el DataGridView después de matar los procesos
+            MostrarProcesosActivos(dataGridView1);
+        }
+
+        // Aquí van las funciones previamente definidas
+
+        public string ObtenerNumeroSerieDisco(string cUnidad)
+        {
+            ManagementObjectSearcher oSearcher = new ManagementObjectSearcher("SELECT * FROM Win32_DiskDrive");
+            foreach (ManagementObject oDisco in oSearcher.Get())
+            {
+                return oDisco["SerialNumber"].ToString();
+            }
+            return "No se encontró el número de serie.";
+        }
+
+        public int nContarUnidadesDisco()
+        {
+            return DriveInfo.GetDrives().Length;
+        }
+
+        public void ObtenerInventarioSistema()
+        {
+            // Obtener información del procesador
+            ManagementObjectSearcher oSearcher = new ManagementObjectSearcher("SELECT * FROM Win32_Processor");
+            foreach (ManagementObject oProcessor in oSearcher.Get())
+            {
+                MessageBox.Show("Procesador: " + oProcessor["Name"]);
+            }
+
+            // Obtener información de la RAM
+            oSearcher = new ManagementObjectSearcher("SELECT * FROM Win32_PhysicalMemory");
+            foreach (ManagementObject oMemoria in oSearcher.Get())
+            {
+                MessageBox.Show("RAM: " + (Convert.ToInt64(oMemoria["Capacity"]) / (1024 * 1024 * 1024)) + " GB");
+            }
+
+            // Obtener información de las tarjetas de red (NIC)
+            oSearcher = new ManagementObjectSearcher("SELECT * FROM Win32_NetworkAdapter WHERE NetEnabled = true");
+            foreach (ManagementObject oNIC in oSearcher.Get())
+            {
+                MessageBox.Show("NIC: " + oNIC["Name"]);
+            }
+
+            // Obtener información de los parches instalados (HotFixes)
+            oSearcher = new ManagementObjectSearcher("SELECT * FROM Win32_QuickFixEngineering");
+            foreach (ManagementObject oPatch in oSearcher.Get())
+            {
+                MessageBox.Show("Patch: " + oPatch["HotFixID"]);
+            }
+        }
+
+        public string ObtenerMacAddress()
+        {
+            foreach (NetworkInterface oNIC in NetworkInterface.GetAllNetworkInterfaces())
+            {
+                if (oNIC.OperationalStatus == OperationalStatus.Up)
+                {
+                    return oNIC.GetPhysicalAddress().ToString();
+                }
+            }
+            return "No se encontró MAC Address.";
+        }
+
+        // Mostrar los procesos activos en el DataGridView
+        public void MostrarProcesosActivos(DataGridView dgv)
+        {
+            dgv.Rows.Clear();
+            dgv.Columns.Clear();
+
+            dgv.Columns.Add("ProcessID", "ID Proceso");
+            dgv.Columns.Add("ProcessName", "Nombre del Proceso");
+            dgv.Columns.Add("MemoryUsage", "Memoria (MB)");
+
+            Process[] procesos = Process.GetProcesses();
+            foreach (Process proceso in procesos)
+            {
+                string memoriaUso = (proceso.WorkingSet64 / (1024 * 1024)).ToString();
+                dgv.Rows.Add(proceso.Id, proceso.ProcessName, memoriaUso);
+            }
+        }
+
+        public void MatarProceso(int nId)
+        {
+            try
+            {
+                Process.GetProcessById(nId).Kill();
+            }
+            catch (Exception oEx)
+            {
+                MessageBox.Show($"Error al matar el proceso: {oEx.Message}");
+            }
+        }
+    }
+}
